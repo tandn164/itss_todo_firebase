d753fe9cc7546fd26e8426bd85eb4d6ce3a620e0
tandn164 <kimphong2910@Kims-MacBook-Pro.local>
2021-05-21 18:52:21 +0700

    fix bug firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGcuh_AgWraIi48BJ6i_8JnL6IuWtKLOg",
-  authDomain: "fir-sample-52db0.firebaseapp.com",
-  projectId: "fir-sample-52db0",
-  storageBucket: "fir-sample-52db0.appspot.com",
-  messagingSenderId: "436477464399",
-  appId: "1:436477464399:web:16146f48501e681ac9ff3a"
+  apiKey: "AIzaSyBJ25Zoj0vvH2Rgs8-2CNe5eohLhXJgxWY",
+  authDomain: "fir-sample-5f95d.firebaseapp.com",
+  projectId: "fir-sample-5f95d",
+  storageBucket: "fir-sample-5f95d.appspot.com",
+  messagingSenderId: "615341003790",
+  appId: "1:615341003790:web:b7a9d1ceafc9a2522a185f"
 };
 
   // Initialize Firebase
